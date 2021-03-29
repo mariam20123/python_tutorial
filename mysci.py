9e2b39ea685411ca8fa9a943a257de2fd04f4a71
unknown <mermahuallpa@gmail.com>
2021-03-28 22:19:43 -0500

    Reading windspeed in too

--- mysci.py
+++ mysci.py
@@ -1,8 +1,8 @@
 #Colum names and column indices to read
-columns = {'date':0,'time':1,'tempout':2}
+columns = {'date':0,'time':1,'tempout':2, 'windspeed':7}
 
 #Data types for each column (only if non-string)
-types = {'tempout': float}
+types = {'tempout': float, 'windspeed':float}
 
 #Initialize my data variable
 data = {}
@@ -15,7 +15,8 @@ with open(filename, 'r')as datafile:
 
     #read the first three lines (header)
     for _ in range(3):
-        datafile.readline()
+        headerline = datafile.readline()
+
     #read and parse the rest og the file
     for line in datafile:
         split_line = line.split()
